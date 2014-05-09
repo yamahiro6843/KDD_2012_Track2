f1ab6af0c67d1f900c79c226f88bff9e0fced971
yasaichi <yasaichi.g@gmail.com>
2014-05-09 11:00:28 +0900

    solution.csvを結合して整形するshell scriptを実装

---
+++ join_solution.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo 'sed -i.bak -e "1d" KDD_Track2_solution.csv'
+sed -i.bak -e "1d" KDD_Track2_solution.csv
+
+echo "paste KDD_Track2_solution.csv test.txt"
+paste KDD_Track2_solution.csv test.txt > test_full_not_formatted.txt
+
+echo "formatting..."
+more test_full_not_formatted.txt | awk -F "[,\t]" -v OFS="|" '{ print $1,$2,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$3 }' > test_full.txt
+
+echo "finished..."
+rm test_full_not_formatted.txt
+mv KDD_Track2_solution.csv.bak KDD_Track2_solution.csv
