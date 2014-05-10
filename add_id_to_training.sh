007864fe70ad4f155e939b68631b12560753a48a
yasaichi <yasaichi.g@gmail.com>
2014-05-10 14:00:48 +0900

    awkの記述方法で気になった点を修正

--- add_id_to_training.sh
+++ add_id_to_training.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "formatting..."
-more training.txt | awk -F "[,\t]" -v OFS="|" '{ $13=""; print NR,substr($0,0,length($0)-1) }' > training_with_id.txt
+awk -F "[,\t]" -v OFS="|" '{ $13=""; print NR,substr($0,0,length($0)-1) }' training.txt > training_with_id.txt 
 
 echo "finished..."
