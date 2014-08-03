174eaef56c99e321fca11fdfcbd620c05e50c063
yasaichi <yasaichi.g@gmail.com>
2014-08-04 00:40:44 +0900

    広告に使用されているトーケンを抽出するスクリプトを追加

---
+++ additional_data/extract_token_ids.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+names=("keyword" "title" "description")
+
+temp_output="token_ids.tmp"
+final_output="token_ids.txt"
+sql=""
+
+for name in ${names[@]}; do
+  sql+="SELECT token_id FROM ${name}_id_token_id GROUP BY token_id;"
+done
+
+echo "fetch token_ids from track2.sqlite3 ..."
+sqlite3 ../track2.sqlite3 <<EOS
+.header off
+.output ${temp_output}
+${sql}
+EOS
+
+Rubyスクリプトでtoken_idをユニークにする
+echo "ruby script start ..."
+irb <<EOS
+  input = File.join("`pwd`", "${temp_output}")
+  output = File.join("`pwd`", "${final_output}")
+  token_ids = []
+
+  IO.foreach(input) { |id| token_ids << id.chop.to_i }
+  File.open(output, "wb") { |f| f.write token_ids.uniq.sort.join("\n") }
+
+  exit
+EOS
+
+rm "${temp_output}"
+
+
