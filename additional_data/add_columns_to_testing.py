db6923646707c61f90cdc3f753545203fea27b54
yasaichi <yasaichi.g@gmail.com>
2014-08-30 11:07:56 +0900

    テストデータのテーブルにもユーザー情報を追加

--- additional_data/add_columns_to_testing.py
+++ additional_data/add_columns_to_testing.py
@@ -11,19 +11,19 @@ con = sqlite3.connect(os.path.join(base_path, "track2.sqlite3"))
 con.row_factory = sqlite3.Row
 
 # age, genderカラムを追加
-column_names = pd.read_sql("PRAGMA TABLE_INFO(training)", con)["name"].values
+column_names = pd.read_sql("PRAGMA TABLE_INFO(testing)", con)["name"].values
 if "gender" not in column_names:
-    con.execute("ALTER TABLE training ADD COLUMN gender integer")
+    con.execute("ALTER TABLE testing ADD COLUMN gender integer")
 if "age" not in column_names:
-    con.execute("ALTER TABLE training ADD COLUMN age integer")
+    con.execute("ALTER TABLE testing ADD COLUMN age integer")
 
 # データをセット
 for row in con.execute("SELECT * FROM users"):
-    query = "UPDATE training SET gender = %d, age = %d WHERE user_id = %d;" % (row["gender"], row["age"], row["id"])
+    query = "UPDATE testing SET gender = %d, age = %d WHERE user_id = %d;" % (row["gender"], row["age"], row["id"])
     print(query)
     con.execute(query)
 
 # インデックスを作成
 print("create index ...")
-con.execute("CREATE INDEX training_age ON training(age)")
+con.execute("CREATE INDEX testing_age ON testing(age)")
 
